2f70215174cef08df4380564da9be432fbf5278b
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-04-16 15:41:03 -0700

    Update Main.java
    added exercise 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -91,26 +91,56 @@ public class Main {
 
 ///////////////////////////////////---- Exercise 3 ----///////////////////////////////////
 
-        System.out.println("Enter game Score: ");
-        String gameScore = scanner.nextLine();
+//        System.out.println("Enter game Score: ");
+//        String gameScore = scanner.nextLine();
+//
+//        int colonIndex = gameScore.indexOf(":");
+//        int pipeIndex = gameScore.indexOf("|");
+//        int secondColonIndex = gameScore.indexOf(":", pipeIndex);
+//        String team1 = gameScore.substring(0,colonIndex);
+//        String team2 = gameScore.substring(colonIndex + 1, pipeIndex);
+//        int team1Score = Integer.parseInt(gameScore.substring(pipeIndex + 1,secondColonIndex));
+//        int team2Score = Integer.parseInt(gameScore.substring(secondColonIndex + 1));
+//
+//
+//        if (team1Score > team2Score){
+//            System.out.println(team1 + " has won the game");
+//        } else if (team2Score > team1Score) {
+//            System.out.println(team2 + " has beaten " + team1);
+//        }
+//        else {
+//            System.out.println("Draw");
+//        }
 
-        int colonIndex = gameScore.indexOf(":");
-        int pipeIndex = gameScore.indexOf("|");
-        int secondColonIndex = gameScore.indexOf(":", pipeIndex);
-        String team1 = gameScore.substring(0,colonIndex);
-        String team2 = gameScore.substring(colonIndex + 1, pipeIndex);
-        int team1Score = Integer.parseInt(gameScore.substring(pipeIndex + 1,secondColonIndex));
-        int team2Score = Integer.parseInt(gameScore.substring(secondColonIndex + 1));
+
+///////////////////////////////////---- Exercise 4 ----///////////////////////////////////
 
 
-        if (team1Score > team2Score){
-            System.out.println(team1 + " has won the game");
-        } else if (team2Score > team1Score) {
-            System.out.println(team2 + " has beaten " + team1);
-        }
-        else {
-            System.out.println("Draw");
-        }
+//        System.out.println("Please enter your full name:");
+//        String fullName = scanner.nextLine();
+//
+//        System.out.println("What date will you be coming (MM/dd/yyyy):");
+//        String date = scanner.nextLine();
+//
+//        System.out.println("How many tickets will you be purchasing:");
+//        int ticket = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        int indexForFullName = fullName.indexOf(" ");
+//        String firstName = fullName.substring(0, indexForFullName);
+//        String lastName = fullName.substring(indexForFullName+1);
+//        String formattedName = lastName + ", " + firstName;
+//
+//        int indexBeforeDay = date.indexOf("/");
+//        int indexAfterDay = date.indexOf("/", indexBeforeDay+1);
+//
+//        String monthOfEvent = date.substring(0, indexBeforeDay);
+//        String dayOfEvent = date.substring(indexBeforeDay+1,indexAfterDay-1);
+//        String yearOfEvent = date.substring(indexAfterDay+1);
+//        String formattedDate = String.format("%s-%s-%s",yearOfEvent,monthOfEvent,dayOfEvent);
+//
+//        System.out.println(ticket + " tickets('s) reserved for " + formattedDate + " under " + formattedName);
+
 
 
     }
